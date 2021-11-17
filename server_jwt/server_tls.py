15fca4e1c52e78bcb78bc97e537aa168bef211cc
Rafael Alejandro García Rodríguez <alexgarohotmailcom@gmail.com>
2021-11-17 18:40:18 +0100

    Quitando prints de debug.

--- server_jwt/server_tls.py
+++ server_jwt/server_tls.py
@@ -10,10 +10,7 @@ usuarios = [{"email":"esp32@correo.com","password":"esp32pass"}]
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server = ssl.wrap_socket(
-    server, server_side=True, keyfile="certs/emqx.key", certfile="certs/emqx.pem"
-)
+server = ssl.wrap_socket(server, server_side=True, keyfile="certs/emqx.key", certfile="certs/emqx.pem")
 
 if __name__ == "__main__":
     server.bind((HOST, PORT))
@@ -22,7 +19,7 @@ if __name__ == "__main__":
     while True:
         connection, client_address = server.accept()
         data = connection.recv(1024)
-        print(data)
+        
         if not data:
             break
         usuario = json.loads(data.decode('utf-8'))
@@ -30,6 +27,5 @@ if __name__ == "__main__":
         for usu in usuarios:
             if usu.get('email')== usuario['email'] and usu.get('password') == usuario['password']:
                 encoded = jwt.encode(usuario, "ciberfisicos1234-secreto", algorithm="HS256")
-                print(encoded)
                 connection.send(encoded.encode('utf-8'))
 
